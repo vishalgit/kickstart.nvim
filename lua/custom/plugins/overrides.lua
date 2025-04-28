389f4b217b1072c451ee2a20ee264cf93db67982
Vishal <vishal.reply@gmail.com>
2025-04-28 17:04:42 +0000

    Added the override file for base config

---
+++ lua/custom/plugins/overrides.lua
@@ -0,0 +1,17 @@
+-- Override the global options and keymaps
+return {
+  {
+    -- This is a dummy local plugin
+    dir = vim.fn.stdpath 'config' .. '/lua/custom/plugins',
+    name = 'overrides',
+    event = 'VeryLazy',
+    config = function()
+      vim.keymap.set('i', 'jk', '<Esc>', { desc = 'Exit insert mode' })
+      vim.keymap.set('v', 'jk', '<Esc>', { desc = 'Exit insert mode' })
+      -- Override options
+      vim.g.have_nerd_font = true
+      vim.opt.relativenumber = true
+      vim.opt.undofile = false
+    end,
+  },
+}
