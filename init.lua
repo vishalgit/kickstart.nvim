d0929c80147f2cdd3d32fcdff74df1d702572cd4
Vishal <vishal.reply@gmail.com>
2025-12-21 12:19:35 +0000

    Added mini.jump

--- init.lua
+++ init.lua
@@ -934,6 +934,8 @@ require('lazy').setup({
         return '%2l:%-2v'
       end
 
+      -- Simple and easy jumps to any part of the file
+      require('mini.jump').setup()
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
