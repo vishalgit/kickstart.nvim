0e7bbfc459e57b6537a6cc5c42abdb9548f0ba70
Vishal <vishal.reply@gmail.com>
2025-12-23 16:57:10 +0530

    Added new plugin

--- init.lua
+++ init.lua
@@ -948,6 +948,7 @@ require('lazy').setup({
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ignore_install = { 'org' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
---
+++ lua/custom/plugins/orgmode.lua
@@ -0,0 +1,14 @@
+-- nvim-orgmode / orgmode
+-- https://github.com/nvim-orgmode/orgmode
+
+return {
+  'nvim-orgmode/orgmode',
+  event = 'VeryLazy',
+  ft = { 'org' },
+  config = function()
+    require('orgmode').setup {
+      org_agenda_files = '~/notes/**/*',
+      org_default_notes_file = '~/notes/refile.org',
+    }
+  end,
+}
