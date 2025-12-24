4ab703b8393e8fe426b569b03e8fe78b9dec6ea0
Vishal <vishal.reply@gmail.com>
2025-12-25 03:00:59 +0530

    Added new GitIgnore plugin

---
+++ lua/custom/plugins/gitignore.lua
@@ -0,0 +1,9 @@
+-- wintermute-cell/gitignore
+-- https://github.com/wintermute-cell/gitignore.nvim
+
+return {
+  'wintermute-cell/gitignore.nvim',
+  config = function()
+    require 'gitignore'
+  end,
+}
