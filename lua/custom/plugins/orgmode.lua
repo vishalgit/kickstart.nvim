33569cc7fd00430f12802f3bb97df35a5af4e05d
Vishal <vishal.reply@gmail.com>
2026-02-22 15:21:04 +0530

    Added telescope refiling

--- lua/custom/plugins/orgmode.lua
+++ lua/custom/plugins/orgmode.lua
@@ -16,6 +16,16 @@ return {
       require('telescope').load_extension 'orgmode'
       require('org-bullets').setup()
       vim.lsp.enable 'org'
+      -- Keybindings
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'org',
+        group = vim.api.nvim_create_augroup('orgmode_config', { clear = true }),
+        callback = function()
+          local buf = vim.api.nvim_get_current_buf()
+          -- Telescope refile much better
+          vim.keymap.set('n', '<leader>or', require('telescope').extensions.orgmode.refile_heading, { buffer = buf, desc = 'Org: Refile' })
+        end,
+      })
     end,
   },
 }
