43f95172c932dc1e7417d7c77638f124e60db751
Vishal Saxena <vishal.reply@gmail.com>
2026-06-25 22:22:28 +0530

    Added better-escape and oil

---
+++ lua/custom/plugins/better-escape.lua
@@ -0,0 +1,3 @@
+vim.pack.add { "https://github.com/max397574/better-escape.nvim" }
+require("better_escape").setup()
+-- vim: ts=2 sts=2 sw=2 et
---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,3 @@
+vim.pack.add {"https://github.com/stevearc/oil.nvim"}
+require("oil").setup()
+-- vim: ts=2 sts=2 sw=2 et
