53f69af8c87fdbfd4394b4d1b15a6b6d5b110195
Vishal Saxena <vishal.reply@gmail.com>
2026-07-04 00:07:09 +0530

    Updated typo

--- lua/custom/plugins/rust.lua
+++ lua/custom/plugins/rust.lua
@@ -1,7 +1,7 @@
 vim.pack.add { 'https://github.com/mrcjkb/rustaceanvim' }
 vim.pack.add { 'https://github.com/saecki/crates.nvim' }
 require('crates').setup({
-  lsp = {enabled = true, actions = true, completion  s= true, hover = true }
+  lsp = {enabled = true, actions = true, completion = true, hover = true }
 })
 vim.g.rustaceanvim = {
   server = {
