e3ab587b327ff00af35b19a8adb4685450297be6
Vishal Saxena <vishal.reply@gmail.com>
2026-07-03 23:50:06 +0530

    Added modelines

--- lua/custom/plugins/rust.lua
+++ lua/custom/plugins/rust.lua
@@ -1,11 +1,12 @@
-vim.pack.add { "https://github.com/mrcjkb/rustaceanvim" }
+vim.pack.add { 'https://github.com/mrcjkb/rustaceanvim' }
 vim.g.rustaceanvim = {
-	server = {
-		default_settings = {
-			['rust-analyzer'] = {
-				check = { command = 'clippy' },
-				cargo = { allFeatures = true },
-			},
-		},
-	},
+  server = {
+    default_settings = {
+      ['rust-analyzer'] = {
+        check = { command = 'clippy' },
+        cargo = { allFeatures = true },
+      },
+    },
+  },
 }
+-- vim: ts=2 sts=2 sw=2 et
