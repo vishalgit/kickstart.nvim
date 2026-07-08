3fd8fc28f9018745f42ec71b5f039a294e4ecfd0
Vishal Saxena <vishal.reply@gmail.com>
2026-07-09 00:43:17 +0530

    Added hidden file viewer

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,3 +1,7 @@
 vim.pack.add {"https://github.com/stevearc/oil.nvim"}
-require("oil").setup()
+require("oil").setup({
+  view_options = {
+    show_hidden = true,
+  },
+})
 -- vim: ts=2 sts=2 sw=2 et
